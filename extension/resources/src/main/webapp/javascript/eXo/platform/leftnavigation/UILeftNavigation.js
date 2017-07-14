1cb89e81f403263c0a5c59e017bf91ba8fa7a23d
Nguyen The Tuyen <tuyennt@exoplatform.com>
2017-07-14 17:17:33 +0700

    PLF-7302 fix perfectScrollbar when resize window

--- extension/resources/src/main/webapp/javascript/eXo/platform/leftnavigation/UILeftNavigation.js
+++ extension/resources/src/main/webapp/javascript/eXo/platform/leftnavigation/UILeftNavigation.js
@@ -1,15 +1,26 @@
-(function ($) {
+(function ($, base) {
   var UILeftNavigation = {
     init: function () {
       var $leftNavi = $('#LeftNavigation');
       this.resize();
-      $leftNavi.perfectScrollbar();
+      if ($(window).width()  > 1024) {
+        $leftNavi.perfectScrollbar();
+      }
+      base.Browser.addOnResizeCallback('UILeftNavigation', function() {
+        var $leftNavi = $('#LeftNavigation');
+        if ($(window).width()  > 1024) {
+          alert('update');
+          $leftNavi.css('position', 'fixed').perfectScrollbar();
+        } else {
+          alert('destroy');
+          $leftNavi.css('position', 'static').perfectScrollbar('destroy');
+        }
+      });
     },
     resize: function () {
       var $leftNavi = $('#LeftNavigation');
       var myHeight = $(window).height() - 44;
       $leftNavi.css('height', myHeight + 'px');
-      $leftNavi.css('overflow', 'hidden');
     }
   };
   return UILeftNavigation;
